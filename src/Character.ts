1c0ab0a91694c9a8c9cc80e5af00b6d7ee6b8e79
Vinicius Gomes <viniciusemog@hotmail.com>
2022-08-09 22:00:38 -0300

    feat: (Req 09) Creates Monster class

--- src/Character.ts
+++ src/Character.ts
@@ -1,6 +1,6 @@
 import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
-import Fighter from './Fighter';
+import Fighter, { SimpleFighter } from './Fighter';
 import Race, { Elf } from './Races';
 import getRandomInt from './utils';
 
@@ -68,7 +68,7 @@ class Character implements Fighter {
     return this._lifePoints;
   }
 
-  attack(enemy: Fighter) {
+  attack(enemy: SimpleFighter) {
     enemy.receiveDamage(this._strength);
   }
 
